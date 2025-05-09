76868fbd8eaf867395ff7e8e698beb71dd2b4e1a
Benjamín <k@iakzs.is-a.dev>
2025-05-09 17:07:37 -0400

    ...

--- env-var.ts
+++
@@ -1,6 +0,0 @@
-﻿export const OPENAIAPIKEY = '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'
-export const OCRAPIKEY = 'K86008509188957'
-
-/*
-   Using this instead of a .env is cooler
- */
